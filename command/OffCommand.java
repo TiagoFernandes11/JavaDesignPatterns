7c5a9ffa1f707e07552182109e2d08288f5b6f38
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-21 12:05:14 -0300

    Command Pattern

---
+++ command/OffCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+public class OffCommand implements Command{
+    Television television;
+
+    OffCommand(Television television){
+        this.television = television;
+    }
+
+    @Override
+    public void execute() {
+        television.off();
+    }
+}
---
+++ command/OnCommand.java
@@ -0,0 +1,14 @@
+package command;
+
+public class OnCommand implements Command{
+    Television television;
+
+    OnCommand(Television television){
+        this.television = television;
+    }
+
+    @Override
+    public void execute() {
+        television.on();
+    }
+}
