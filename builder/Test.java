97448afc8549a1653d07e09aa96b7085d1a19d5f
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-22 17:16:14 -0300

    Builder pattern refactor

--- builder/Test.java
+++ builder/Test.java
@@ -2,9 +2,6 @@ package builder;
 
 public class Test {
     public static void main(String[] args) {
-        HttpClient uglyClient = new HttpClient("Get", "http://test.com", null, null, "headers", "body");
-        System.out.println(uglyClient);
-
         HttpClient client = new HttpClient.HttpClientBuilder().method("Get").url("http://test.com").secure(null, null).headers("headers").body("body").build();
         System.out.println(client);
     }
