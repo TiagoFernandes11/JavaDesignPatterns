cf4c11e1512b175727983915fcd575fdba8088f2
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-03-22 18:17:14 -0300

    Refactoring

--- facade/OrderProcessor.java
+++ facade/OrderProcessor.java
@@ -2,16 +2,16 @@ package facade;
 
 public class OrderProcessor {
     public boolean checkStock(String name){
-        System.out.println("Checking stock");
+        System.out.println("Checking stock for " + name);
         return true;
     }
 
     public int placeOrder(String name, int quantity){
-        System.out.println("Order placed");
+        System.out.println("Order placed: " + name + " qtd: " + quantity);
         return 1;
     }
 
     public void shipOrder(int orderId){
-        System.out.println("Order shipped");
+        System.out.println("Order shipped. Id: " + orderId);
     }
 }
